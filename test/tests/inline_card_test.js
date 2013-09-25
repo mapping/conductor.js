8a0606ae6045ab515d25d21b67b789e14bcdef30
David J. Hamilton <davidjh@hjdivad.com>
2013-09-25 12:04:37 -0700

    wrap inline_tests and kill them

--- test/tests/inline_card_test.js
+++ test/tests/inline_card_test.js
@@ -1,44 +1,48 @@
+(function(){
+
 var conductor, card, qunitFixture;
 
 
-// This is an advanced feature.  Please be EXTREMELY careful before using inline
-// cards.
-module('Inline Conductor Cards', {
-  setup: function() {
-    conductor = newConductor();
-    qunitFixture = document.getElementById('qunit-fixture');
-    $(qunitFixture).html();
-  }
-});
+// // This is an advanced feature.  Please be EXTREMELY careful before using inline
+// // cards.
+// module('Inline Conductor Cards', {
+//   setup: function() {
+//     conductor = newConductor();
+//     qunitFixture = document.getElementById('qunit-fixture');
+//     $(qunitFixture).html();
+//   }
+// });
 
-test("Conductor cards can be loaded with the inline adapter", function() {
-  // one assertion will come from the card's activation
-  expect(3);
-  stop();
-  card = conductor.load("/test/fixtures/test_card.js", 1, { adapter: Conductor.adapters.inline });
+// test("Conductor cards can be loaded with the inline adapter", function() {
+//   // one assertion will come from the card's activation
+//   expect(3);
+//   stop();
+//   card = conductor.load("/test/fixtures/test_card.js", 1, { adapter: Conductor.adapters.inline });
 
-  card.appendTo(qunitFixture);
-  card.render();
+//   card.appendTo(qunitFixture);
+//   card.render();
 
-  equal($(qunitFixture).find('iframe:first').length, 0, "the iframe adapter was not used");
-  equal($(qunitFixture).find('div:first').length, 1, "the inline adapter was used");
-});
+//   equal($(qunitFixture).find('iframe:first').length, 0, "the iframe adapter was not used");
+//   equal($(qunitFixture).find('div:first').length, 1, "the inline adapter was used");
+// });
 
-test("Cards loaded with the inline adapter can still `conductor.require` resources", function() {
-  // card require, card activation
-  expect(2);
-  stop();
+// test("Cards loaded with the inline adapter can still `conductor.require` resources", function() {
+//   // card require, card activation
+//   expect(2);
+//   stop();
 
-  card = conductor.load('/test/fixtures/load_card.js', 1, { adapter: Conductor.adapters.inline });
-  card.appendTo(qunitFixture);
-});
+//   card = conductor.load('/test/fixtures/load_card.js', 1, { adapter: Conductor.adapters.inline });
+//   card.appendTo(qunitFixture);
+// });
 
-test("Cards loaded with the inline adapter can still `conductor.require` cross-domain resources", function() {
-  // card require, card activation
-  expect(2);
-  stop();
+// test("Cards loaded with the inline adapter can still `conductor.require` cross-domain resources", function() {
+//   // card require, card activation
+//   expect(2);
+//   stop();
 
-  /*global crossOrigin */
-  card = conductor.load(crossOrigin + '/test/fixtures/load_card.js', 1, { adapter: Conductor.adapters.inline });
-  card.appendTo(qunitFixture);
-});
+//   /*global crossOrigin */
+//   card = conductor.load(crossOrigin + '/test/fixtures/load_card.js', 1, { adapter: Conductor.adapters.inline });
+//   card.appendTo(qunitFixture);
+// });
+
+})();
